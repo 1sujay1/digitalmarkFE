45410930f09ddec6b49c5fea55bff8a611fc1248
logesh-aimwindow <Logeshwaran.p@aimwindow.co.in>
2025-05-11 17:51:56 +0530

    implement quantity adjustment for cart items and enhance cart synchronization with server

--- js/config.js
+++ js/config.js
@@ -1,1 +1,19 @@
-const BASE_URL = "http://localhost:5000"
+ const BASE_URL = "http://localhost:5000";
+
+ const getToken = () => {
+    const token = localStorage.getItem('token');
+    return token !== null && token !== undefined ? token : '';
+  }
+
+   const isLoggedIn = () => {
+    const token = localStorage.getItem('token');
+    return token !== null && token !== undefined;
+  }
+  const setLocalCartITems = (items) => {
+    localStorage.setItem('cart', JSON.stringify(items));
+  }
+    const getLocalCartItems = () => {
+        const items = localStorage.getItem('cart');
+        return items ? JSON.parse(items) : [];
+    }
+    
